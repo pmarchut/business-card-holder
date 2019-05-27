ea10b854644c1da36a9b2893968717d4a76d09c9
pmarchut <pm9701133@gmail.com>
2019-05-27 22:32:14 +0200

    add responsive search form

--- BusinessCardHolder/ViewModels/EmployeeIndexViewModel.cs
+++ BusinessCardHolder/ViewModels/EmployeeIndexViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessCardHolder.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,21 @@ namespace BusinessCardHolder.ViewModels
     public class EmployeeIndexViewModel
     {
         public List<Employee> Employees { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        public string JobTitle { get; set; }    
+
+        [Display(Name = "Job Title")]
+        public string JobTitle { get; set; }
+        
         public string Telephone { get; set; }
+
+        [Display(Name = "Mobile Telephone")]
         public string MobileTelephone { get; set; }
+
         public string Email { get; set; }
     }
 }
--- BusinessCardHolder/ViewModels/FirmIndexViewModel.cs
+++ BusinessCardHolder/ViewModels/FirmIndexViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessCardHolder.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,20 @@ namespace BusinessCardHolder.ViewModels
     public class FirmIndexViewModel
     {
         public List<Firm> Firms { get; set; }
+
         public string Name { get; set; }
+
         public string NIP { get; set; }
+
         public string Address { get; set; }
+
         public string City { get; set; }
+
+        [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
+
         public string Telephone { get; set; }
+
         public string Email { get; set; }
     }
 }
